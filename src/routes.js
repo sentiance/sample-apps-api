0c556eb531b95e01e8affd194050d29f75a2149c
Prakash Raman <prakash.raman@sentiance.com>
2022-06-20 11:01:49 +0000

    Merged in feature/API-2150_sdk_auth20 (pull request #6)
    * API-2150: /auth/code for SDK Auth 2.0
    
    Approved-by: Hassan Shakeel

--- src/routes.js
+++ src/routes.js
@@ -37,6 +37,44 @@ router.get(
 )
 
 /**
+ * Retrieves an authentication_code from the Sentiance Platform.
+ *
+ * An "external_id" is required to be sent to the Sentiance Platform as part of the
+ * authentication_code request. Ensure to replace the "CURRENT_USER_ID" with the
+ * current/logged_in user_id.
+ *
+ *
+ * This authentication_code must be used to setup the Sentiance SDK.
+ */
+router.get(
+  '/auth/code',
+  asyncWrapper(async function (_req, res) {
+    try {
+      const reqBody = { external_id: 'CURRENT_USER_ID' }
+
+      const response = await axios.post(`${Config.sentiance_api_base_url}/users/auth-code`, reqBody, {
+        headers: {
+          Authorization: `Bearer ${Config.app.user_link_api_key}`,
+        },
+      })
+
+      res.json({
+        auth_code: response.data.authentication_code,
+        platform_url: Config.sentiance_api_base_url,
+      })
+    } catch (err) {
+      throw boomify(err, {
+        statusCode: err?.response?.status,
+        message: err?.response?.data?.message,
+        decorate: {
+          errorCode: err?.response?.data?.errorCode,
+        },
+      })
+    }
+  })
+)
+
+/**
  * Links a user id of the app to the Sentiance id of the user
  * This route accepts a json body with property 'user_id' which is the user id of the app
  *
